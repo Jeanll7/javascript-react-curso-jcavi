4ebe8796996623293ef3839ac5a24a54f0a932de
devjean <jeancarloleall@gmail.com>
2022-11-26 10:57:53 -0300

    exercicio10

--- index10.js
+++ index10.js
@@ -1,27 +1,35 @@
 // Faça um programa que receba a idade, o peso e o sexo de 10 pessoas. Calcule e imprima:
-const pesos = [62, 72, 83, 64, 70, 66, 77, 88, 81, 110]
-const idadesGeral = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
-const genero = ['M', 'F', 'M', 'F', 'M', 'F', 'M', 'F', 'M', 'F']
+// total de homens;
+// média das idades dos homens;
+// total de mulheres;
+// média dos pesos das mulheres.
+let pesos = [62, 72, 83, 64, 70, 66, 77, 88, 81, 110]
+let idades = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+let generos = ['M', 'F', 'M', 'F', 'M', 'F', 'M', 'F', 'M', 'F']
 
-let pesoSoma = 0
-let idadeSoma = 0
-let idadeMedia = 0
-let qtde = 0
+let totalHomens = 0
+let totalIdadeHomens = 0
+let totalMulheres = 0
+let mediaPessoMulheres = 0
 
-for (let i = 0; i < genero.length; i++) {
-    if (genero[i] === seletor) {
-        qtde++
-        pesoSoma += pesos[i]
-        idadeSoma += idadesGeral[i]
+for (let i = 0; i < generos.length; i++) {
+    const genero = generos[i];
+    const idade = idades[i];
+    const peso = pesos[i];
+
+    if (genero === 'M') {
+        totalIdadeHomens += idade
+        totalHomens++
+    }
+    if (genero === 'F') {
+        mediaPessoMulheres += peso
+        totalMulheres++
     }
 }
 
-alert(`
-    HOMENS São ${homens[3]} Homens 
-    Media Idade: ${homens[2]} anos
-`)
-
-alert(`
-    MULHERES São ${mulheres[3]} 
-    Mulheres Média de Pesos:  ${mulheres[0]}kg
-`)
+console.log(`
+    Total de homens ${totalHomens} 
+    Total idade homens ${totalIdadeHomens} anos
+    Total de mulheres ${totalMulheres} 
+    Média dos pesos das mulheres ${mediaPessoMulheres}kg
+    `)
