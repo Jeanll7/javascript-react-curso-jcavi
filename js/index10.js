ffd5dff2253da0906c40af63f2754b6fe4c536a3
devjean <jeancarloleall@gmail.com>
2022-11-26 13:40:18 -0300

    push1

--- js/index10.js
+++ js/index10.js
@@ -3,7 +3,7 @@
 // média das idades dos homens;
 // total de mulheres;
 // média dos pesos das mulheres.
-let pesos = [62, 72, 83, 64, 70, 66, 77, 88, 81, 110]
+let pesoTotal = [62, 72, 83, 64, 70, 66, 77, 88, 81, 110]
 let idades2 = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
 let generos = ['M', 'F', 'M', 'F', 'M', 'F', 'M', 'F', 'M', 'F']
 
@@ -15,7 +15,7 @@ let mediaPessoMulheres = 0
 for (let i = 0; i < generos.length; i++) {
     const genero = generos[i];
     const idade = idades2[i];
-    const peso = pesos[i];
+    const peso = pesoTotal[i];
 
     if (genero === 'M') {
         totalIdadeHomens += idade
