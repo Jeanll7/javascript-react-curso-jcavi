25df7996ec72f57b61f153762e1a8005e0d7182d
devjean <jeancarloleall@gmail.com>
2022-11-26 14:11:00 -0300

    repo1

---
+++ exercicios02/func/index01.js
@@ -0,0 +1,1 @@
+// 1- Crie uma função que receba dois números como parâmetro, retorne a soma dos valores, em seguida execute a função mostrando na tela o seguinte texto: 'A soma dos valores é ${(resultado)}'.
---
+++ exercicios02/func/index03.js
@@ -0,0 +1,1 @@
+// 3 - Crie uma função que receba duas strings como parâmetro, primeiro nome e segundo nome, em seguida retorne o nome completo do indivíduo.
