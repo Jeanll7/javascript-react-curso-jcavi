da75a2823a02d997aed202ba73df29674942b863
devjean <jeancarloleall@gmail.com>
2022-11-27 10:15:25 -0300

    repo7

--- exercicios02/func/index01.js
+++ exercicios02/func/index01.js
@@ -1,7 +1,9 @@
 // 1- Crie uma função que receba dois números como parâmetro, retorne a soma dos valores, em seguida execute a função mostrando na tela o seguinte texto: 'A soma dos valores é ${(resultado)}'.
 
+
+
 function somar(n1, n2) {
     return n1 + n2
 }
 
-console.log(`A soma dos valores é: ${somar(3, 3)}`)
+console.log(`A soma dos valores é: ${somar(3, 4)}`)
--- exercicios02/func/index02.js
+++ exercicios02/func/index02.js
@@ -1,5 +1,14 @@
 // 2- Crie uma função que receba um array de números (crie uma segunda função para validar os dados dentro do array) e depois calcule a soma de  todos os valores dentro do array. Execute a função mostrando na tela o seguinte texto: 'A soma dos valores do array é ${(resultado)}'.
 
+function Somar(array) {
+    let total = 0
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= 2) {
+            total += array[i]
+        }
+    }
+    return total
+}
+let array = [10, 12, 15, 23]
 
-
-// console.log(`A soma dos valores do array é ${arrayNumeros(total)}`)
+console.log(`A soma dos valores do array é ${Somar(array)}`)
