2a8f056fbd6ba2548e5fb2131bed498a1fe1b1cf
devjean <jeancarloleall@gmail.com>
2022-11-28 11:03:35 -0300

    repo13

--- exercicios02/func/index02.js
+++ exercicios02/func/index02.js
@@ -1,6 +1,6 @@
 // 2- Crie uma função que receba um array de números (crie uma segunda função para validar os dados dentro do array) e depois calcule a soma de  todos os valores dentro do array. Execute a função mostrando na tela o seguinte texto: 'A soma dos valores do array é ${(resultado)}'.
 
-function Somar(array) {
+function somar2(array) {
     let total = 0
     for (let i = 0; i < array.length; i++) {
         if (array[i] >= 1) {
@@ -11,4 +11,4 @@ function Somar(array) {
 }
 let array = [10, 5, 10, 2]
 
-console.log(`A soma dos valores do array é ${Somar(array)}`)
+console.log(`A soma dos valores do array é ${somar2(array)}`)
--- exercicios02/func/index05.js
+++ exercicios02/func/index05.js
@@ -1,1 +1,16 @@
 // 5- Crie uma função que receba um array de números (Valide os dados dentro do array, reaproveite a função criada no exercício 1) e depois calcule a média de todos os valores dentro do array. Execute a função mostrando na tela o seguinte texto: 'A média dos valores do array é ${(resultado)}'.
+
+// let numeros = [7, 15, 21, 20, 18, 35, 82, 19, 8, 1]
+// let soma = 0
+
+// function somarArray(numeros) {
+
+//     for (let i = 0; i < numeros.length; i++) {
+//         if (numeros > somaArr) {
+//             somaArr += numeros
+//         }
+//         // soma += numeros[i]
+//     }
+//     return soma
+// }
+// console.log(`A média dos valores do array é ${numeros}`)
