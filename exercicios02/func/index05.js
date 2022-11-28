560558602f989859e08318b5a7771503c5ab5ea9
devjean <jeancarloleall@gmail.com>
2022-11-28 13:00:52 -0300

    repo13

--- exercicios02/func/index05.js
+++ exercicios02/func/index05.js
@@ -1,6 +1,6 @@
 // 5- Crie uma função que receba um array de números (Valide os dados dentro do array, reaproveite a função criada no exercício 1) e depois calcule a média de todos os valores dentro do array. Execute a função mostrando na tela o seguinte texto: 'A média dos valores do array é ${(resultado)}'.
 
-let numArray = [10, 20, 30]
+let numArray = [2, 7, 15]
 
 function mediaSomaArr(numeros) {
     let total = 0
