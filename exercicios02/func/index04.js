ea87a6964ab29a24eb53c7def07335deae0fb799
devjean <jeancarloleall@gmail.com>
2022-11-28 10:32:54 -0300

    repo10

--- exercicios02/func/index04.js
+++ exercicios02/func/index04.js
@@ -1,11 +1,13 @@
 // 4- Crie uma função que receba um string como parâmetro  e execute um "windows.prompt" com o texto recebido como parâmetro, e em seguida retorne o valor que o usuário digitou.
 
 
-// let nomeUsuario = prompt('Digite seu nome!');
-// let nomeInvalido = null
 
-// if (nomeUsuario === nomeUsuario || nomeInvalido) {
-//     alert(`Seu nome é ${nomeUsuario}`);
-// } else {
-//     alert(`Seu nome não pode ser numero ${nomeInvalido}`)
-// }
+
+function dizerNome(nome) {
+    if (nomeUsuario === nomeUsuario || nomeInvalido) {
+        alert(`Seu nome é ${nomeUsuario}`);
+    }
+}
+let nomeUsuario = prompt('Digite seu nome!');
+
+dizerNome(nomeUsuario)
