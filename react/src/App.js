3d54489f64446ca72d81055f90ecd203785745f6
devjean <jeancarloleall@gmail.com>
2022-12-11 13:46:44 -0300

    react

---
+++ react/src/App.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [carro, setCarro] = useState();
+  const [placa, setPlaca] = useState();
+
+  const [veiculos, setVeiculos] = useState([]);
+
+  function cadastrarVeiculo() {
+    const veiculo = {
+      carro,
+      placa,
+    };
+    veiculos.push(veiculo);
+    setVeiculos([...veiculos]);
+    limparForm();
+  }
+
+  function limparForm() {
+    setCarro("");
+    setPlaca("");
+  }
+
+  // function verificarCastrado() {}
+
+  return (
+    <>
+      <main className="container">
+        <div className="form">
+          <h1>Cadastrar Veiculo</h1>
+          <input
+            placeholder="Digite nome do veiculo"
+            value={carro}
+            onChange={(e) => {
+              setCarro(e.target.value);
+            }}
+          />
+          <input
+            placeholder="Digite a placa"
+            value={placa}
+            onChange={(e) => {
+              setPlaca(e.target.value);
+            }}
+          />
+          <button className="button" onClick={cadastrarVeiculo}>
+            Cadastrar
+          </button>
+        </div>
+        <div className="table-main">
+          <table className="table">
+            <tr className="tr">
+              <th>Nome Veiculo -</th>
+              <th>Placa</th>
+            </tr>
+            {veiculos.map((veiculo) => {
+              return (
+                <tr className="tr">
+                  <td>{veiculo.carro}</td>
+                  <td>{veiculo.placa}</td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
